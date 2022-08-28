388ad54995eb60008daff46eea0831a1ec4c5be3
ckymn <cokyamanmuhammet@gmail.com>
2022-08-28 20:43:18 +0300

    code fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
